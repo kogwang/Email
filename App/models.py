832ca2bfa2c413437abb5b3ac34a0dcfa340a3c5
kog <366986211@qq.com>
2018-12-15 19:12:45 +0800

    蓝图框架搭建，数据库连接，前端内容展示

--- App/models.py
+++ App/models.py
@@ -3,6 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Person(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    name = db.Column(db.String(20),unique=True)
+    username = db.Column(db.String(20),unique=True)
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    time = db.Column(db.String(100))
+    key = db.Column(db.Boolean,default=True)
+
+
+
--- App/views.py
+++ App/views.py
@@ -1,39 +1,71 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+
+from App.models import User
 
 blue = Blueprint('email',__name__)
 
 #登录界面
-@blue.route('/')
+@blue.route('/',methods=['GET',])
 def login():
     return render_template('./login.html')
 
 #index网页
-@blue.route('/index/')
+@blue.route('/index/',methods=['GET',])
 def index():
     return render_template('./index.html')
 
 #右侧统计内容
-@blue.route('/welcome/')
+@blue.route('/welcome/',methods=['GET',])
 def welcome():
     return render_template('./welcome.html')
 
 #用户列表
-@blue.route('/user-list/')
+@blue.route('/user-list/',methods=['GET',])
 def user_list():
     return render_template('./user-list.html')
 
 #已删除用户
-@blue.route('/user-del/')
+@blue.route('/user-del/',methods=['GET',])
 def user_del():
     return render_template('./user-del.html')
 
 #邮件模版
-@blue.route('/email-model/')
+@blue.route('/email-model/',methods=['GET',])
 def email_model():
     return render_template('./email_model.html')
 
 #收件人列表编辑
-@blue.route('/user-list/user-edit.html')
+@blue.route('/user-list/user-edit.html',methods=['GET',])
 def user_edit():
     return render_template('./user-edit.html')
+
+#测试接口
+@blue.route('/test/')
+def test11():
+    user_names=User.query.all()
+    data={
+        'usernames':user_names
+    }
+    return render_template('./1111111.html',user_names=user_names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
